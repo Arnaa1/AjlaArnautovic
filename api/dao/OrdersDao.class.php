4fc8d291c9b7383cd103e6bc4494c94dc317edf5
Ajla Arnautovic <ajla.arnautovic@socialexplorer.com>
2021-04-20 16:33:33 +0200

    Dao classes

---
+++ api/dao/OrdersDao.class.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once dirname(__FILE__)."/BaseDao.class.php";
+class CartDao extends BaseDao{
+
+  public function get_orders_by_id($id){
+    return $this->query_unique("SELECT * FROM orders, WHERE id=:id", ["id=>$id"]);
+  }
+
+
+  public function get_orders_by_name($name){
+    return $this->query_unique("SELECT * FROM orders, WHERE name=:name", ["name"=>name]);
+  }
+
+  public function get_all_orders(){
+    return $this->query ("SELECT * FROM orders", []);
+  }
+
+}
+
+ ?>
--- api/dao/ProductsDao.class.php
+++ api/dao/ProductsDao.class.php
@@ -2,14 +2,29 @@
 
 class ProductsDao extends BaseDao{
 
-public function add_product($peoduct){
+  private $table="product";
+
+    public function get_product_by_id($id){
+      return $this->query_unique("SELECT * FROM products, WHERE id=:id", ["id"=>$id]);
+    }
+
+    public function get_product_by_name($name){
+      return $this->query_unique("SELECT * FROM products, WHERE name=:name", ["name"=>name]);
+    }
+
+    public function get_all_products(){
+      return $this->query ("SELECT * FROM products", []);
+    }
+
+
+  public function add_product($entity){
+  return $this->insert($this->table, $entity);
+  }
+
+  public function update_product($id, $product){
+    $this->update("product", $id, $product, "id");
+  }
 
   
 }
-
-
-}
-
-
-
- ?>
+?>
